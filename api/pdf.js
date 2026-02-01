4c0909beb946422be446a77b0b0f42af90ee2b5e
chha1994-gif <chha1994@hotmail.com>
2026-02-01 02:21:45 +0100

    Create pdf.js

---
+++ api/pdf.js
@@ -0,0 +1,24 @@
+import PDFDocument from "pdfkit";
+
+export default async function handler(req, res) {
+  const data = req.body;
+
+  const doc = new PDFDocument();
+  const buffers = [];
+
+  doc.on("data", buffers.push.bind(buffers));
+  doc.on("end", () => {
+    const pdf = Buffer.concat(buffers);
+    res.setHeader("Content-Type", "application/pdf");
+    res.send(pdf);
+  });
+
+  doc.fontSize(20).text("Tilbud", { underline: true });
+  doc.moveDown();
+
+  Object.entries(data).forEach(([key, value]) => {
+    doc.text(${key}: ${value});
+  });
+
+  doc.end();
+}
