476d94830d76152503b2ef1c35ac773500a4cae9
chha1994-gif <chha1994@hotmail.com>
2026-02-25 00:54:34 +0100

    Update check-pro.js

--- api/check-pro.js
+++ api/check-pro.js
@@ -47,8 +47,7 @@ module.exports = async function handler(req, res) {
     const validStatuses = ["active", "trialing"];
 
 const isPro = subscriptions.data.some(sub =>
-  validStatuses.includes(sub.status) &&
-  !sub.cancel_at_period_end
+  validStatuses.includes(sub.status)
 );
 
     return res.status(200).json({
